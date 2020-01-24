17253ab89128230dfa127b7c50693803e49d5ba9
Joe Bush <arcandio@gmail.com>
2020-01-24 15:52:38 -0600

    spiffed up the tree view

--- GwData.py
+++ GwData.py
@@ -16,15 +16,25 @@ class GwData():
 
 	def SetProjectPath(self, projectPath):
 		self.app.model = QFileSystemModel()
+		self.app.model.setNameFilters(['*.md'])
+		self.app.model.setNameFilterDisables(False)
 		self.app.model.setRootPath(projectPath)
 		print('treeview model root path: ', self.app.model.rootPath())
 		self.app.treeView_2.setModel(self.app.model)
 		self.app.treeView_2.setRootIndex(self.app.model.index(projectPath))
+		# configure tree view
+		self.app.treeView_2.hideColumn(1)
+		self.app.treeView_2.hideColumn(2)
+		self.app.treeView_2.hideColumn(3)
 
 	def OpenMarkdownFile(self, signal):
 		self.openFilePath=self.app.model.filePath(signal)
-		print('open markdown file: ', self.openFilePath)
-		with open(self.openFilePath) as f:
-			c = f.read()
-			self.app.text.md = c
-			self.app.text.mh()
+		# todo change this to a swtich case based on the type of file clicked
+		try:
+			with open(self.openFilePath) as f:
+				print('open markdown file: ', self.openFilePath)
+				c = f.read()
+				self.app.text.md = c
+				self.app.text.mh()
+		except:
+			print('clicked a folder, not a file')
