defe3663e54484091f5d5f72e227871fb788e642
Joe Bush <arcandio@gmail.com>
2020-01-25 14:19:41 -0600

    selecting block, converting block working

--- GwParse.py
+++ GwParse.py
@@ -5,9 +5,6 @@ class GwParse():
 	def __init__(self, app):
 		self.app = app
 		self.md = ''
-		# cannot use below, causes recursion, use key press instead
-		self.app.textEdit.textChanged.connect(self.HtmlEdited)
-		#self.mainButton.clicked.connect(self.HtmlEdited)
 	
 	def mh(self):
 		html = markdown.markdown(self.md)
@@ -17,10 +14,22 @@ class GwParse():
 		html = self.app.textEdit.toHtml()
 		self.md = html2markdown.convert(html)
 
-	def HtmlEdited(self):
+	def ParseChunk(self, html):
+		md = html2markdown.convert(html)
+		html = markdown.markdown(md)
+		stripped = self.StripP(html)
+		return stripped
+
+	def StripP(self, html):
+		html = html.replace('<p>', '', 1)
+		html = html.replace('</p>', '', -1)
+		return html
+"""
+	def HtmlEditedOld(self):
 		cur = self.app.textEdit.textCursor()
 		self.app.textEdit.blockSignals(True)
 		self.hm()
 		self.mh()
 		self.app.textEdit.blockSignals(False)
 		self.app.textEdit.setTextCursor(cur)
+"""
--- GwText.py
+++ GwText.py
@@ -1,16 +1,59 @@
-from PyQt5 import QtGui
+"""
+QTextEdit https://doc.qt.io/qt-5/qtextedit.html
+	textCursor / QTextCursor
+	insertPlainText
+	insertHtml
+	append
+	paste
 
+QTextCursor https://doc.qt.io/qt-5/qtextcursor.html
+	selectedText
+	selection
+	deleteChar
+	deletePreviousChar
+	removeSelectedText
+	createList
+	mergeCharFormat
+	insertText
+	insertList
+	insertTable
+	insertImage
+	movePosition
+	https://www.riverbankcomputing.com/static/Docs/PyQt4/qtextcursor.html#MoveOperation-enum
+
+QTextCharFormat
+	setFontWeight
+
+https://doc.qt.io/qt-5/richtext.html
+
+"""
+from PyQt5 import QtGui
 
 class GwText():
 	def __init__(self,app):
 		self.app = app
 		self.te = self.app.textEdit
-		self.cur = self.app.textEdit.textCursor()
 		self.app.actionBold.triggered.connect(self.Bold)
+		# cannot use below, causes recursion, use key press instead
+		self.app.textEdit.textChanged.connect(self.GetPara)
+		self.app.mainButton.clicked.connect(self.GetPara)
 
 	def Bold(self):
 		print('makebold')
 		f = QtGui.QTextCharFormat()
 		f.setFontWeight(QtGui.QFont.Bold)
-		self.cur.mergeCharFormat(f)
-		# doesn't work yet
+		cur = self.app.textEdit.textCursor()
+		cur.mergeCharFormat(f)
+
+	def DebugSelected(self):
+		cur = self.app.textEdit.textCursor()
+		print('cursor.selectedText:', cur.selectedText())
+
+	def GetPara(self):
+		cur = self.app.textEdit.textCursor()
+		cur.movePosition(QtGui.QTextCursor.StartOfBlock)
+		cur.movePosition(QtGui.QTextCursor.EndOfBlock, QtGui.QTextCursor.KeepAnchor)
+		html = self.app.parser.ParseChunk(cur.selectedText())
+		print('cursor.selectedText:', cur.selectedText())
+		print('reconverted html:', html)
+		
